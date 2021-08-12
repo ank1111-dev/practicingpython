1978e79b09d44e256a051c080aa2a12b3807208e
ank1111-dev <62601460+ank1111-dev@users.noreply.github.com>
2021-08-12 19:46:36 +1000

    Add files via upload

---
+++ spinayarn.py
@@ -0,0 +1,8 @@
+str1= input("Enter an adjective:")
+str2= input("Enter a proper noun:").capitalize()
+str3= input("Enter a preposition:")
+str4= input("Enter a verb:")
+
+str= str2 + " " + str3 + " " + str1 + " " + str4 + "."
+
+print(str)
